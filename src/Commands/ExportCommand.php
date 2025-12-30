ac0c22c8e54edb1ad200ffa1a996e8da8c89dca7
teiling88 <teiling88@gmail.com>
2025-12-30 15:04:10 +0100

    #6 - fix phpstan

--- src/Commands/ExportCommand.php
+++ src/Commands/ExportCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Batteryincluded\BatteryincludedBundle\Commands;
 
+use BatteryIncludedSdk\Dto\ProductBaseDto;
 use BatteryIncludedSdk\Service\SyncService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -26,14 +27,14 @@ class ExportCommand extends Command
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $transactionId = 'Transaction_' . time();
-
+        $batch[] = new ProductBaseDto('1337'); // to avoid "undefined variable" error
         foreach ($this->provider as $provider) {
             foreach ($provider->getBatches(500) as $batch) {
                 $this->syncService->syncFullBatchElements($transactionId, false, ...$batch);
             }
             $output->writeln(get_class($provider) . ' export done, committing...');
-            $this->syncService->syncFullBatchElements($transactionId, true, ...$batch);
         }
+        $this->syncService->syncFullBatchElements($transactionId, true, ...$batch);
         $output->writeln('<info>Export done.</info>');
 
         return Command::SUCCESS;
